0957c4ff27d6ae750542f0c74b923a8406451a6a
lendo <keilbaum@web.de>
2019-09-15 11:35:10 +0200

    init gta importer

---
+++ __init__.py
@@ -0,0 +1,78 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name" : "GTA V Model importer (.otd)",
+    "author" : "Lendo Keilbaum",
+    "description" : "Import GTA V models (.otd)",
+    "location": "File > Import",
+    "blender" : (2, 80, 0),
+    "version" : (0, 0, 1),
+    "location" : "",
+    "warning" : "",
+    "category" : "Import"
+}
+
+if "bpy" in locals():
+    import importlib
+    if "import_mesh" in locals():
+        importlib.reload(import_mesh)
+else:
+    from . import import_mesh
+
+
+
+import bpy
+from bpy.props import (
+        BoolProperty,
+        EnumProperty,
+        FloatProperty,
+        StringProperty,
+        )
+from bpy_extras.io_utils import (
+        ImportHelper,
+        ExportHelper,
+        axis_conversion,
+        )
+
+
+
+class ImportOTD(bpy.types.Operator, ImportHelper):
+    """Import from OTD file format (.otd)"""
+    bl_idname = "import_scene.otd"
+    bl_label = 'Import otd'
+    bl_options = {'UNDO'}
+
+    filename_ext = ".mesh"
+
+    def execute(self, context):
+        keywords = self.as_keywords()
+        return import_mesh.load(self, context, **keywords)
+
+# Add to a menu
+def menu_func_import(self, context):
+    self.layout.operator(ImportOTD.bl_idname, text="OTD Mesh (.mesh)")
+
+
+def register():
+    bpy.utils.register_class(ImportOTD)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+
+
+def unregister():
+    bpy.utils.unregister_class(ImportOTD)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+
+
+if __name__ == "__main__":
+    register()
---
+++ import_mesh.py
@@ -0,0 +1,114 @@
+import bpy
+import os
+import bmesh
+import re
+from copy import deepcopy
+
+from mathutils import (
+        Euler,
+        Matrix,
+        Vector,
+        )
+
+
+
+
+def getNameFromFile(filepath):
+    return os.path.basename(filepath)
+
+# index_header_pattern = re.compile(r"(.*) Indices\s*(.*?) .*", re.VERBOSE)
+opening_bracket = re.compile(r"\t{3}\{+")
+closing_bracket = re.compile(r"\t{3}\}+")
+index_header_pattern = re.compile(r"\s+ Indices \s+ (?P<index_count>\d+)", re.VERBOSE)
+index_line_pattern = re.compile(r"\t{4}(?P<indices>(?:\d+[^\.]){1,15})",re.VERBOSE)
+
+
+vertex_header_pattern = re.compile(r"\s+ Vertices \s+ (?P<vertex_count>\d+)", re.VERBOSE)
+
+number_pattern = "([\-\+]?\d+(?:\.\d+)?)"
+vertex_line_pattern = re.compile(r"""
+\t{4}
+(?P<pos>(?:(?:[\-\+]?\d*(?:\.\d*)?)\s+){3}) \/\s
+(?P<weights>(?:(?:[\-\+]?\d*(?:\.\d*)?)\s+){4}) \/\s
+(?P<undef1>(?:(?:[\-\+]?\d*(?:\.\d*)?)\s+){4}) \/\s
+(?P<normal>(?:(?:[\-\+]?\d*(?:\.\d*)?)\s+){3}) \/\s
+(?P<undef2>(?:(?:[\-\+]?\d*(?:\.\d*)?)\s+){4}) \/\s
+(?P<uv>(?:(?:[\-\+]?\d*(?:\.\d*)?)\s+){2}) \/\s
+(?P<undef3>(?:(?:[\-\+]?\d*(?:\.\d*)?)\s+){4})
+""", re.VERBOSE)
+
+
+vertex_line_pattern2 = re.compile(r"\t+(?P<pos>(?:(?:[\-\+]?\d*(?:\.\d*)?)\s+){3}) \/", re.VERBOSE)
+
+def load_Mesh(filepath):
+    Meshes = []
+    Mesh = []
+    Indices = []
+    read_indices = False
+    Vertices = []
+    read_vertices = False
+
+
+    skinned = False
+    bone_count = 0
+    weights = []
+    with open(filepath, 'r') as file:
+        for line in file.readlines():
+            if not read_indices:
+                # find begining of index list
+                match = index_header_pattern.match(line)
+                if match:
+                    print("index count: {0}".format(match.group("index_count")))
+                    read_indices = True
+                    Mesh = []
+                    Indices = []
+            elif read_indices:
+                if closing_bracket.match(line):
+                    read_indices = False
+                # reading the indices
+                match = index_line_pattern.match(line)
+                if match and read_indices:
+                    match_i = match.group("indices")
+                    temp_list = list(int(i) for i in match_i.split())
+                    # print(temp_list)
+                    Indices.extend(temp_list)
+            if not read_indices and not read_vertices:
+                match = re.search(vertex_header_pattern, line)
+                if match:
+                    print("vertex count: {0}".format(match.group("vertex_count")))
+                    read_vertices = True
+                    Vertices = []
+            elif read_vertices:
+                if closing_bracket.match(line):
+                    read_vertices = False
+                    Mesh.append(deepcopy(Indices))
+                    Mesh.append(deepcopy(Vertices))
+                    Meshes.append(deepcopy(Mesh))
+                match = vertex_line_pattern.match(line)
+                if match and read_vertices:
+                    pos = Vector(float(p) for p in match.group("pos").split())
+                    weights = Vector(float(p) for p in match.group("weights").split())
+                    normal = Vector(float(p) for p in match.group("normal").split())
+                    uv = Vector(float(p) for p in match.group("uv").split())
+                    Vertices.append((pos, weights, normal, uv))
+                    # print(match.group("pos"))
+                    # print("pos: {0}, weights: {1}, normal: {2}, uv:{3}".format(match.group("pos"), match.group("weights"),match.group("normal"),match.group("uv")))
+
+    # create meshes
+    for num, m in enumerate(Meshes):
+        faces = []
+        for i in range(int(len(m[0])/3)):
+            f = i*3
+            faces.append([m[0][f], m[0][f+1], m[0][f+2]])
+        name = getNameFromFile(filepath)+str(num)
+        Mesh_data = bpy.data.meshes.new(name)
+        verts = list(v[0] for v in m[1])
+        Mesh_data.from_pydata(verts, (), faces)
+        Obj = bpy.data.objects.new(name, Mesh_data)
+        bpy.context.scene.collection.objects.link(Obj)
+
+
+def load(operator, context, filepath=""):
+    print("importing: {0}".format(filepath))
+    load_Mesh(filepath)
+    return {'FINISHED'}
